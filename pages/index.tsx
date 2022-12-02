26973ec243bae9475e5a20cf71cc262102e4d3fb
Tural Hajiyev <thajiyev9@outlook.com>
2022-12-02 19:31:39 +0400

    Update variable names

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,40 +3,40 @@ import styles from '../styles/Home.module.css';
 import Confetti from 'react-confetti';
 
 export default function Home() {
-  const [weekDay, setWeekDay] = useState<string>('salam');
+  const [title, setTitle] = useState<string>('salam');
   const [description, setDescription] = useState<string>('sagol');
   useEffect(() => {
     const date = new Date();
     const weekDay = date.getDay();
     switch (weekDay) {
       case 0:
-        setWeekDay('Yox 😑');
+        setTitle('Yox 😑');
         setDescription('Sabahın bazar ertəsi olma qorxusu...');
         break;
       case 1:
-        setWeekDay('Yox 🤬');
+        setTitle('Yox 🤬');
         setDescription('Həftənin ən pis günü...');
         break;
       case 2:
-        setWeekDay('Yox 🥲');
+        setTitle('Yox 🥲');
         setDescription('Həftənin ən pis gününə yaxın gün. (daha yaxşı)');
         break;
       case 3:
-        setWeekDay('Yox 🫣');
+        setTitle('Yox 🫣');
         setDescription(
           'Məktəb vaxtı həftənin ən çətin dərslərinin olduğu gün.'
         );
         break;
       case 4:
-        setWeekDay('Yox 🫡');
+        setTitle('Yox 🫡');
         setDescription('Möhtəşəm günə son gün.');
         break;
       case 5:
-        setWeekDay('Hə 😍🥰😘');
+        setTitle('Hə 😍🥰😘');
         setDescription('Bu gün o gündür, bu gün həftənin ən möhtəşəm günüdür.');
         break;
       case 6:
-        setWeekDay('Yox 🫠');
+        setTitle('Yox 🫠');
         setDescription('Möhtəşəm günün səhəri.');
         break;
     }
@@ -44,9 +44,9 @@ export default function Home() {
 
   return (
     <div className={styles.Home}>
-      <div className={styles['not-friday-title']}>{weekDay}</div>
+      <div className={styles['not-friday-title']}>{title}</div>
       <div style={{ fontSize: '20px' }}>{description}</div>
-      {weekDay.split(' ')[0] === 'Hə' && <Confetti />}
+      {title.split(' ')[0] === 'Hə' && <Confetti />}
     </div>
   );
 }
